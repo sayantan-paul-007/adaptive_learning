8f1413f3dccac10d01d026ea0ce871c04526f373
Sayantan Paul <sayantanpaul222@gmail.com>
2023-12-23 21:22:40 +0530

    new changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,65 +6,65 @@ const Footer = () => {
   return (
     <footer className='bg-primary-700'>
               <div
-            class="container flex flex-col flex-wrap px-5 py-10 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
-            <div class="flex-shrink-0 w-64 mx-auto flex justify-center md:mx-0 md:text-left">
+            className="container flex flex-col flex-wrap px-5 py-10 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
+            <div className="flex-shrink-0 w-64 mx-auto flex justify-center md:mx-0 md:text-left">
             <img src={logo1} height='128' width='128' alt="" />
             </div>
-            <div class="flex flex-wrap flex-grow mt-10 -mb-10 text-center md:pl-20 md:mt-0 md:text-left">
-                <div class="w-full px-4 lg:w-1/4 md:w-1/2">
-                    <h2 class="mb-3 text-lg font-semibold font-montserrat tracking-widest text-gray-900 uppercase">About</h2>
-                    <nav class="mb-10 list-none">
-                        <li class="mt-3">
-                            <a href='www.google.com' class="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Company</a>
+            <div className="flex flex-wrap flex-grow mt-10 -mb-10 text-center md:pl-20 md:mt-0 md:text-left">
+                <div className="w-full px-4 lg:w-1/4 md:w-1/2">
+                    <h2 className="mb-3 text-lg font-semibold font-montserrat tracking-widest text-gray-900 uppercase">About</h2>
+                    <nav className="mb-10 list-none">
+                        <li className="mt-3">
+                            <a href='www.google.com' className="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Company</a>
                         </li>
-                        <li class="mt-3">
-                            <a href='www.google.com' class="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Careers</a>
+                        <li className="mt-3">
+                            <a href='www.google.com' className="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Careers</a>
                         </li>
-                        <li class="mt-3">
-                            <a href='www.google.com' class="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Blog</a>
-                        </li>
-                    </nav>
-                </div>
-                <div class="w-full px-4 lg:w-1/4 md:w-1/2">
-                    <h2 class="mb-3 text-lg font-semibold font-montserrat tracking-widest text-gray-900 uppercase title-font">Support</h2>
-                    <nav class="mb-10 list-none">
-                        <li class="mt-3">
-                            <a href='www.google.com' class="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Contact Support</a>
-                        </li>
-                        <li class="mt-3">
-                            <a href='www.google.com' class="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Help Resources</a>
-                        </li>
-                        <li class="mt-3">
-                            <a href='www.google.com' class="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Release Updates</a>
+                        <li className="mt-3">
+                            <a href='www.google.com' className="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Blog</a>
                         </li>
                     </nav>
                 </div>
-                <div class="w-full px-4 lg:w-1/4 md:w-1/2">
-                    <h2 class="mb-3 text-lg font-semibold font-montserrat tracking-widest text-gray-900 uppercase title-font">Platform
+                <div className="w-full px-4 lg:w-1/4 md:w-1/2">
+                    <h2 className="mb-3 text-lg font-semibold font-montserrat tracking-widest text-gray-900 uppercase title-font">Support</h2>
+                    <nav className="mb-10 list-none">
+                        <li className="mt-3">
+                            <a href='www.google.com' className="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Contact Support</a>
+                        </li>
+                        <li className="mt-3">
+                            <a href='www.google.com' className="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Help Resources</a>
+                        </li>
+                        <li className="mt-3">
+                            <a href='www.google.com' className="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Release Updates</a>
+                        </li>
+                    </nav>
+                </div>
+                <div className="w-full px-4 lg:w-1/4 md:w-1/2">
+                    <h2 className="mb-3 text-lg font-semibold font-montserrat tracking-widest text-gray-900 uppercase title-font">Platform
                     </h2>
-                    <nav class="mb-10 list-none">
-                        <li class="mt-3">
-                            <a href='www.google.com' class="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Terms &amp; Privacy</a>
+                    <nav className="mb-10 list-none">
+                        <li className="mt-3">
+                            <a href='www.google.com' className="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Terms &amp; Privacy</a>
                         </li>
-                        <li class="mt-3">
-                            <a href='www.google.com' class="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Pricing</a>
+                        <li className="mt-3">
+                            <a href='www.google.com' className="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Pricing</a>
                         </li>
-                        <li class="mt-3">
-                            <a href='www.google.com' class="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">FAQ</a>
+                        <li className="mt-3">
+                            <a href='www.google.com' className="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">FAQ</a>
                         </li>
                     </nav>
                 </div>
-                <div class="w-full px-4 lg:w-1/4 md:w-1/2">
-                    <h2 class="mb-3 text-lg font-semibold font-montserrat tracking-widest text-gray-900 uppercase title-font">Contact</h2>
-                    <nav class="mb-10 list-none">
-                        <li class="mt-3">
-                            <a href='www.google.com' class="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Send a Message</a>
+                <div className="w-full px-4 lg:w-1/4 md:w-1/2">
+                    <h2 className="mb-3 text-lg font-semibold font-montserrat tracking-widest text-gray-900 uppercase title-font">Contact</h2>
+                    <nav className="mb-10 list-none">
+                        <li className="mt-3">
+                            <a href='www.google.com' className="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Send a Message</a>
                         </li>
-                        <li class="mt-3">
-                            <a href='www.google.com' class="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Request a Quote</a>
+                        <li className="mt-3">
+                            <a href='www.google.com' className="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">Request a Quote</a>
                         </li>
-                        <li class="mt-3">
-                            <a href='www.google.com' class="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">+123-456-7890</a>
+                        <li className="mt-3">
+                            <a href='www.google.com' className="text-gray-500 cursor-pointer hover:text-gray-900 font-notosans">+123-456-7890</a>
                         </li>
                     </nav>
                 </div>
@@ -85,16 +85,16 @@ const Footer = () => {
       <div className="w-full py-5">
         <div className="flex justify-center items-center gap-10">
           <a href="https://t.me/adaptivelearning">
-          <svg xmlns="http://www.w3.org/2000/svg" height='32' shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 512"><defs><linearGradient id="prefix__a" gradientUnits="userSpaceOnUse" x1="256" y1="3.84" x2="256" y2="512"><stop offset="0" stop-color="#2AABEE"/><stop offset="1" stop-color="#229ED9"/></linearGradient></defs><circle fill="#000" cx="256" cy="256" r="256"/><path fill="#fff" d="M115.88 253.3c74.63-32.52 124.39-53.95 149.29-64.31 71.1-29.57 85.87-34.71 95.5-34.88 2.12-.03 6.85.49 9.92 2.98 2.59 2.1 3.3 4.94 3.64 6.93.34 2 .77 6.53.43 10.08-3.85 40.48-20.52 138.71-29 184.05-3.59 19.19-10.66 25.62-17.5 26.25-14.86 1.37-26.15-9.83-40.55-19.27-22.53-14.76-35.26-23.96-57.13-38.37-25.28-16.66-8.89-25.81 5.51-40.77 3.77-3.92 69.27-63.5 70.54-68.9.16-.68.31-3.2-1.19-4.53s-3.71-.87-5.3-.51c-2.26.51-38.25 24.3-107.98 71.37-10.22 7.02-19.48 10.43-27.77 10.26-9.14-.2-26.72-5.17-39.79-9.42-16.03-5.21-28.77-7.97-27.66-16.82.57-4.61 6.92-9.32 19.04-14.14z"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" height='32' shapeRendering="geometricPrecision" textRendering="geometricPrecision" imageRendering="optimizeQuality" fillRule="evenodd" clipRule="evenodd" viewBox="0 0 512 512"><defs><linearGradient id="prefix__a" gradientUnits="userSpaceOnUse" x1="256" y1="3.84" x2="256" y2="512"><stop offset="0" stopColor="#2AABEE"/><stop offset="1" stopColor="#229ED9"/></linearGradient></defs><circle fill="#000" cx="256" cy="256" r="256"/><path fill="#fff" d="M115.88 253.3c74.63-32.52 124.39-53.95 149.29-64.31 71.1-29.57 85.87-34.71 95.5-34.88 2.12-.03 6.85.49 9.92 2.98 2.59 2.1 3.3 4.94 3.64 6.93.34 2 .77 6.53.43 10.08-3.85 40.48-20.52 138.71-29 184.05-3.59 19.19-10.66 25.62-17.5 26.25-14.86 1.37-26.15-9.83-40.55-19.27-22.53-14.76-35.26-23.96-57.13-38.37-25.28-16.66-8.89-25.81 5.51-40.77 3.77-3.92 69.27-63.5 70.54-68.9.16-.68.31-3.2-1.19-4.53s-3.71-.87-5.3-.51c-2.26.51-38.25 24.3-107.98 71.37-10.22 7.02-19.48 10.43-27.77 10.26-9.14-.2-26.72-5.17-39.79-9.42-16.03-5.21-28.77-7.97-27.66-16.82.57-4.61 6.92-9.32 19.04-14.14z"/></svg>
           </a>
         <a href="https://www.instagram.com/adaptive_learning/">
-          <svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" height='32' image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 512"><path fill="#000" fill-rule="nonzero" d="M170.663 256.157c-.083-47.121 38.055-85.4 85.167-85.483 47.121-.092 85.407 38.03 85.499 85.16.091 47.129-38.047 85.4-85.176 85.492-47.112.09-85.399-38.039-85.49-85.169zm-46.108.091c.141 72.602 59.106 131.327 131.69 131.186 72.592-.141 131.35-59.09 131.209-131.692-.141-72.577-59.114-131.335-131.715-131.194-72.585.141-131.325 59.115-131.184 131.7zm237.104-137.091c.033 16.953 13.817 30.681 30.772 30.648 16.961-.033 30.689-13.811 30.664-30.764-.033-16.954-13.818-30.69-30.78-30.657-16.962.033-30.689 13.818-30.656 30.773zm-208.696 345.4c-24.958-1.087-38.511-5.234-47.543-8.709-11.961-4.629-20.496-10.178-29.479-19.094-8.966-8.95-14.532-17.46-19.202-29.397-3.508-9.032-7.73-22.569-8.9-47.527-1.269-26.982-1.559-35.077-1.683-103.432-.133-68.339.116-76.434 1.294-103.441 1.069-24.942 5.242-38.512 8.709-47.536 4.628-11.977 10.161-20.496 19.094-29.479 8.949-8.982 17.459-14.532 29.403-19.202 9.025-3.525 22.561-7.714 47.511-8.9 26.998-1.277 35.085-1.551 103.423-1.684 68.353-.132 76.448.108 103.456 1.295 24.94 1.086 38.51 5.217 47.527 8.709 11.968 4.628 20.503 10.144 29.478 19.094 8.974 8.95 14.54 17.443 19.21 29.412 3.524 9 7.714 22.553 8.892 47.494 1.285 26.999 1.576 35.095 1.7 103.433.132 68.355-.117 76.451-1.302 103.441-1.087 24.958-5.226 38.52-8.709 47.561-4.629 11.952-10.161 20.487-19.103 29.471-8.941 8.949-17.451 14.531-29.403 19.201-9.009 3.517-22.561 7.714-47.494 8.9-26.998 1.269-35.086 1.559-103.448 1.684-68.338.132-76.424-.125-103.431-1.294zM149.977 1.773c-27.239 1.285-45.843 5.648-62.101 12.018-16.829 6.561-31.095 15.354-45.286 29.604C28.381 57.653 19.655 71.944 13.144 88.79c-6.303 16.299-10.575 34.912-11.778 62.168C.172 178.264-.102 186.973.031 256.489c.133 69.508.439 78.234 1.741 105.547 1.302 27.231 5.649 45.828 12.019 62.093 6.569 16.83 15.353 31.088 29.611 45.288 14.25 14.201 28.55 22.918 45.404 29.438 16.282 6.295 34.902 10.583 62.15 11.778 27.305 1.203 36.022 1.468 105.521 1.335 69.532-.132 78.25-.439 105.555-1.733 27.239-1.303 45.826-5.665 62.1-12.019 16.829-6.586 31.095-15.353 45.288-29.611 14.191-14.251 22.917-28.55 29.428-45.405 6.304-16.282 10.592-34.903 11.777-62.134 1.195-27.322 1.478-36.048 1.344-105.556-.133-69.516-.447-78.225-1.741-105.523-1.294-27.255-5.657-45.844-12.019-62.118-6.577-16.829-15.352-31.079-29.602-45.287-14.25-14.192-28.55-22.935-45.404-29.429-16.29-6.305-34.903-10.601-62.15-11.779C333.747.164 325.03-.102 255.506.031c-69.507.133-78.224.431-105.529 1.742z"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" shapeRendering="geometricPrecision" textRendering="geometricPrecision" height='32' imageRendering="optimizeQuality" fillRule="evenodd" clipRule="evenodd" viewBox="0 0 512 512"><path fill="#000" fillRule="nonzero" d="M170.663 256.157c-.083-47.121 38.055-85.4 85.167-85.483 47.121-.092 85.407 38.03 85.499 85.16.091 47.129-38.047 85.4-85.176 85.492-47.112.09-85.399-38.039-85.49-85.169zm-46.108.091c.141 72.602 59.106 131.327 131.69 131.186 72.592-.141 131.35-59.09 131.209-131.692-.141-72.577-59.114-131.335-131.715-131.194-72.585.141-131.325 59.115-131.184 131.7zm237.104-137.091c.033 16.953 13.817 30.681 30.772 30.648 16.961-.033 30.689-13.811 30.664-30.764-.033-16.954-13.818-30.69-30.78-30.657-16.962.033-30.689 13.818-30.656 30.773zm-208.696 345.4c-24.958-1.087-38.511-5.234-47.543-8.709-11.961-4.629-20.496-10.178-29.479-19.094-8.966-8.95-14.532-17.46-19.202-29.397-3.508-9.032-7.73-22.569-8.9-47.527-1.269-26.982-1.559-35.077-1.683-103.432-.133-68.339.116-76.434 1.294-103.441 1.069-24.942 5.242-38.512 8.709-47.536 4.628-11.977 10.161-20.496 19.094-29.479 8.949-8.982 17.459-14.532 29.403-19.202 9.025-3.525 22.561-7.714 47.511-8.9 26.998-1.277 35.085-1.551 103.423-1.684 68.353-.132 76.448.108 103.456 1.295 24.94 1.086 38.51 5.217 47.527 8.709 11.968 4.628 20.503 10.144 29.478 19.094 8.974 8.95 14.54 17.443 19.21 29.412 3.524 9 7.714 22.553 8.892 47.494 1.285 26.999 1.576 35.095 1.7 103.433.132 68.355-.117 76.451-1.302 103.441-1.087 24.958-5.226 38.52-8.709 47.561-4.629 11.952-10.161 20.487-19.103 29.471-8.941 8.949-17.451 14.531-29.403 19.201-9.009 3.517-22.561 7.714-47.494 8.9-26.998 1.269-35.086 1.559-103.448 1.684-68.338.132-76.424-.125-103.431-1.294zM149.977 1.773c-27.239 1.285-45.843 5.648-62.101 12.018-16.829 6.561-31.095 15.354-45.286 29.604C28.381 57.653 19.655 71.944 13.144 88.79c-6.303 16.299-10.575 34.912-11.778 62.168C.172 178.264-.102 186.973.031 256.489c.133 69.508.439 78.234 1.741 105.547 1.302 27.231 5.649 45.828 12.019 62.093 6.569 16.83 15.353 31.088 29.611 45.288 14.25 14.201 28.55 22.918 45.404 29.438 16.282 6.295 34.902 10.583 62.15 11.778 27.305 1.203 36.022 1.468 105.521 1.335 69.532-.132 78.25-.439 105.555-1.733 27.239-1.303 45.826-5.665 62.1-12.019 16.829-6.586 31.095-15.353 45.288-29.611 14.191-14.251 22.917-28.55 29.428-45.405 6.304-16.282 10.592-34.903 11.777-62.134 1.195-27.322 1.478-36.048 1.344-105.556-.133-69.516-.447-78.225-1.741-105.523-1.294-27.255-5.657-45.844-12.019-62.118-6.577-16.829-15.352-31.079-29.602-45.287-14.25-14.192-28.55-22.935-45.404-29.429-16.29-6.305-34.903-10.601-62.15-11.779C333.747.164 325.03-.102 255.506.031c-69.507.133-78.224.431-105.529 1.742z"/></svg>
         </a>
         <a href="https://www.youtube.com/channel/UCBrk7LnxpQFyxfLf-2F4arQ">
-        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 640 640"><path d="M633.468 192.038s-6.248-44.115-25.477-63.485c-24.366-25.477-51.65-25.642-64.123-27.118-89.493-6.52-223.904-6.52-223.904-6.52h-.236s-134.352 0-223.893 6.52c-12.52 1.523-39.768 1.63-64.123 27.118-19.24 19.37-25.358 63.485-25.358 63.485S-.012 243.806-.012 295.681v48.509c0 51.768 6.366 103.643 6.366 103.643s6.248 44.114 25.358 63.52c24.355 25.477 56.363 24.65 70.655 27.367 51.237 4.89 217.644 6.366 217.644 6.366s134.529-.237 224.022-6.638c12.52-1.477 39.756-1.63 64.123-27.119 19.24-19.37 25.476-63.532 25.476-63.532S640 396.03 640 344.154v-48.508c-.13-51.769-6.497-103.643-6.497-103.643l-.035.035zm-379.8 211.007V223.173L426.56 313.41l-172.892 89.635z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" shapeRendering="geometricPrecision" textRendering="geometricPrecision" imageRendering="optimizeQuality" fillRule="evenodd" clipRule="evenodd" viewBox="0 0 640 640"><path d="M633.468 192.038s-6.248-44.115-25.477-63.485c-24.366-25.477-51.65-25.642-64.123-27.118-89.493-6.52-223.904-6.52-223.904-6.52h-.236s-134.352 0-223.893 6.52c-12.52 1.523-39.768 1.63-64.123 27.118-19.24 19.37-25.358 63.485-25.358 63.485S-.012 243.806-.012 295.681v48.509c0 51.768 6.366 103.643 6.366 103.643s6.248 44.114 25.358 63.52c24.355 25.477 56.363 24.65 70.655 27.367 51.237 4.89 217.644 6.366 217.644 6.366s134.529-.237 224.022-6.638c12.52-1.477 39.756-1.63 64.123-27.119 19.24-19.37 25.476-63.532 25.476-63.532S640 396.03 640 344.154v-48.508c-.13-51.769-6.497-103.643-6.497-103.643l-.035.035zm-379.8 211.007V223.173L426.56 313.41l-172.892 89.635z"/></svg>
         </a>
         <a href="https://www.facebook.com/adaplearning/">
-        <svg xmlns="http://www.w3.org/2000/svg" height='32' shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 509 509"><g fill-rule="nonzero"><path fill="#000" d="M509 254.5C509 113.94 395.06 0 254.5 0S0 113.94 0 254.5C0 373.86 82.17 474 193.02 501.51V332.27h-52.48V254.5h52.48v-33.51c0-86.63 39.2-126.78 124.24-126.78 16.13 0 43.95 3.17 55.33 6.33v70.5c-6.01-.63-16.44-.95-29.4-.95-41.73 0-57.86 15.81-57.86 56.91v27.5h83.13l-14.28 77.77h-68.85v174.87C411.35 491.92 509 384.62 509 254.5z"/><path fill="#fff" d="M354.18 332.27l14.28-77.77h-83.13V227c0-41.1 16.13-56.91 57.86-56.91 12.96 0 23.39.32 29.4.95v-70.5c-11.38-3.16-39.2-6.33-55.33-6.33-85.04 0-124.24 40.16-124.24 126.78v33.51h-52.48v77.77h52.48v169.24c19.69 4.88 40.28 7.49 61.48 7.49 10.44 0 20.72-.64 30.83-1.86V332.27h68.85z"/></g></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" height='32' shapeRendering="geometricPrecision" textRendering="geometricPrecision" imageRendering="optimizeQuality" fillRule="evenodd" clipRule="evenodd" viewBox="0 0 509 509"><g fillRule="nonzero"><path fill="#000" d="M509 254.5C509 113.94 395.06 0 254.5 0S0 113.94 0 254.5C0 373.86 82.17 474 193.02 501.51V332.27h-52.48V254.5h52.48v-33.51c0-86.63 39.2-126.78 124.24-126.78 16.13 0 43.95 3.17 55.33 6.33v70.5c-6.01-.63-16.44-.95-29.4-.95-41.73 0-57.86 15.81-57.86 56.91v27.5h83.13l-14.28 77.77h-68.85v174.87C411.35 491.92 509 384.62 509 254.5z"/><path fill="#fff" d="M354.18 332.27l14.28-77.77h-83.13V227c0-41.1 16.13-56.91 57.86-56.91 12.96 0 23.39.32 29.4.95v-70.5c-11.38-3.16-39.2-6.33-55.33-6.33-85.04 0-124.24 40.16-124.24 126.78v33.51h-52.48v77.77h52.48v169.24c19.69 4.88 40.28 7.49 61.48 7.49 10.44 0 20.72-.64 30.83-1.86V332.27h68.85z"/></g></svg>
         </a>
         </div>
       </div>
--- src/Components/Youtube.jsx
+++ src/Components/Youtube.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 const Youtube = () => {
     const ytdata = [
         {
+            id:1,
             link:"https://www.youtube.com/watch?v=uM13j_gPDX8&t=163s",
             thumbnail:"https://i3.ytimg.com/vi/uM13j_gPDX8/maxresdefault.jpg",
             title:"Accounts Ch 1 | Partnership Final Accounts | Introduction | HSC | By Rahul Sir | Lecture 1",
@@ -9,6 +10,7 @@ const Youtube = () => {
             year:3
         },
         {
+            id:2,
             link:"https://www.youtube.com/watch?v=vHuGkCcAfXA",
             thumbnail:"https://i3.ytimg.com/vi/vHuGkCcAfXA/maxresdefault.jpg",
             title:"Accounts Ch 1 | Partnership Final Accounts | Illustration 1 Page 24 | HSC | By Rahul Sir | Lecture 2",
@@ -16,6 +18,7 @@ const Youtube = () => {
             year:3
         },
         {
+            id:3,
             link:"https://www.youtube.com/watch?v=klQ6lpJr5pg&t=359s",
             thumbnail:"https://i3.ytimg.com/vi/klQ6lpJr5pg/maxresdefault.jpg",
             title:"Accounts Ch 1 | Partnership Final Accounts | Practical Problem 1 | HSC | By Rahul Sir | Lecture 12",
@@ -28,11 +31,11 @@ const Youtube = () => {
 <h3 className='font-bold text-center text-7xl max-laptop:text-[54px] max-tablet:text-5xl max-mobile:text-4xl font-montserrat pt-4 pb-10'>
                 Find us on Youtube
             </h3>
-            <div className='bg-emerald-600 rounded-xl shadow-lg px-2 pb-10  flex flex-row max-mobile:flex-col justify-around '>
+            <div className='rounded-xl px-2 py-4 border-2 flex flex-row max-mobile:flex-col justify-around '>
                 {
                     ytdata.map((yt) =>(
                         
-                        <div className='flex w-[30%] max-mobile:w-full flex-col font-notosans py-[24px] hover:shadow-xl '>
+                        <div className='flex w-[30%] max-mobile:w-full flex-col font-notosans py-[24px]' key={yt.id}>
                             <a href={yt.link}>
                             <img src={yt.thumbnail} className='h-[200px] rounded-xl' alt='Yt1 Thumbnail'/>
                             <p className='py-2 text-lg text-left font-semibold'>
@@ -49,7 +52,7 @@ const Youtube = () => {
                 }
                 
             </div>
-            <div className='flex justify-center py-8'>
+            <div className='flex justify-center py-6'>
                 <button className='py-4 px-10 rounded-xl font-bold font-notosans text-white bg-red-600 text-xl'>
                         Visit Our Channel
                     </button>
